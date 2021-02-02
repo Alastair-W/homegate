9bf054344f2353f2a66c4b418d4782b8798d9d82
RowdyOnline <rowdy14@gmail.com>
2021-02-01 17:55:00 -0800

    Added csv files

--- homegate.py
+++ homegate.py
@@ -4,9 +4,10 @@ import time
 import requests
 from bs4 import BeautifulSoup
 import math
+import csv
 
 # declare a variable that contains the url to be scraped - as these results will be over mutiple pages this will be the first page
-url = 'https://www.homegate.ch/rent/real-estate/matching-list?loc=geo-region-rive-droite-lac%2Cgeo-region-rive-droite-campagne'
+url = 'https://www.homegate.ch/rent/real-estate/region-nyon/matching-list'
 # use the 'requests' library to make a 'get' call for that webpage
 page = requests.get(url)
 # parse the web page content using the BeautifulSoup library
@@ -72,20 +73,22 @@ scraper()
 # now we loop through the remaining results pages by adding '?ep=' and then the page number that starts at 2
 
 for n in range(2,numPages+1):
-    ind = "ep="+str(n)+"&"
+    ind = "?ep="+str(n)
     # create a new variable with the url being created dynamically as we loop through each results page
-    newUrl = f'https://www.homegate.ch/rent/real-estate/matching-list?{ind}loc=geo-region-rive-droite-lac%2Cgeo-region-rive-droite-campagne'
+    newUrl = f'https://www.homegate.ch/rent/real-estate/region-nyon/matching-list{ind}'
     print(newUrl)
     # Create driver and identify which browser to 'drive'
     driver = webdriver.Firefox()
     # Get a page
     driver.get(newUrl)
     # wait 3 seconds cos sometimes not all elements are ready
-    time.sleep(4)  
+    time.sleep(5)  
     # create a loop that updates the element to scroll to by 1 in each iteration so that we trigger the JS for every listing on the page (there are 20 per page)
     # on the last page stop the loop on the last listing (which is less than 20)
     if n == numPages:
         for i in range(1,lastPage):
+        # wait 1 second cos sometimes not all elements are ready
+            time.sleep(2) 
         # Get element using xPath because cant use a distinct HTML tag with a variable that takes the count of each iteration of the loop
             element = driver.find_element_by_xpath(f'//*[@id="app"]/main/div[2]/div/div[3]/div[2]/div[{i}]/a/div/div[2]')
         # Create action chain object
@@ -94,18 +97,17 @@ for n in range(2,numPages+1):
             driver.execute_script("arguments[0].scrollIntoView(true);",element)  
         # move to each element in the listings to execute the JS and load the content
             action.move_to_element(element).perform()
-        # wait 1 second cos sometimes not all elements are ready
-            time.sleep(1)  
         # parse the whole web page(s) content using the BeautifulSoup library
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         
     else:
         for i in range(1,21):
+            time.sleep(2) 
             element = driver.find_element_by_xpath(f'//*[@id="app"]/main/div[2]/div/div[3]/div[2]/div[{i}]/a/div/div[2]')
             action = ActionChains(driver)
             driver.execute_script("arguments[0].scrollIntoView(true);",element)  
             action.move_to_element(element).perform()
-            time.sleep(1)  
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
     # loop through the page to find the elements with rent information and add that to a list
     propertyInfo = soup.find_all('div', class_=["ListItem_data_18_z_", "ListItemTopPremium_data_3i7Ca"])
     print(f'Number of instances of property data tag: {len(propertyInfo)}')
@@ -127,6 +129,12 @@ myDict['Address'] = addressList
 myDict['Rent'] = rentList
 myDict['Size'] = sizeList
 myDict['Rooms'] = roomList
+
+zd = zip(*myDict.values())
+with open('nyon.csv', 'w') as file:
+    writer = csv.writer(file, delimiter=',')
+    writer.writerow(myDict.keys())
+    writer.writerows(zd)
 
 print(myDict)
 print(len(myDict))
